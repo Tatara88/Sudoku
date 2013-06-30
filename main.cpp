522b389b421918d8dc92110dafd17828e97b74d6
Moises Tadeo <administrator@moisestadeo.com>
2013-06-30 12:12:49 -0700

    added functions to clean code
    added functions to clean code but its a bit buggy when user inputs a
    letter instead of digit. Program needs input validation system.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,27 @@
 using namespace std;
 
 // Function Prototypes
-void displayMain();
-void viewRules();
-void displayGraph(int array[9][9]);
 
-int menuChoiceValidation(int x);
+// Displays Main menu
+void displayMain(int * x);
+// Displays user selection.
+void menuChoiceSelection(int x);
+// Starts the game.
+void startGame();
+// Displays Game Rules.
+void viewRules();
+// View program credits
+void viewCredits();
+// Display current graph.
+void displayGraph(int array[][9]);
+
 
 int main()
 {
-	//menuChoice user selection variable.
+	// menuChoice chooses from 1-3 at the displayMain function.
 	int menuChoice;
 
+	// 
 	int chartArray[9][9] = {
 								{1, 2, 3, 4, 5, 6, 7, 8, 9},
 								{1, 2, 3, 4, 5, 6, 7, 8, 9},
@@ -25,32 +35,77 @@ int main()
 								{1, 2, 3, 4, 5, 6, 7, 8, 9}
 							};
 
-	// Display Graph
-	//displayGraph(chartArray);
 
-	displayMain();
-	cin >> menuChoice;
 
-	system("pause");
+
+	do 
+	{
+		// Display Graph
+		//displayGraph(chartArray);
+
+		// Display the main menu list, pass the argument by reference.
+		displayMain(&menuChoice);
+
+		// User has made a selection, pass the argument by value.
+		menuChoiceSelection(menuChoice);
+
+	} while (menuChoice >=1 && menuChoice <=3);
+
+
+
+	// End of code.
 	return 0;
 
 }
 
 
+
+
+
+
 // Displays Main menu
-void displayMain()
+void displayMain(int * x)
 {
+	system("cls");
 	cout << "Welcome to Sodoku project!" << endl
 		 << "Description HERE........." << endl << endl
 		 << "1.\tPlay" << endl
 		 << "2.\tRules" << endl
 		 << "3.\tCredits" << endl
-		 << "4.\tQuit" << endl << endl
-		 << "Please make a your selection (1-4): ";
+		 << "(Any Other Key)\tQuit" << endl << endl
+		 << "Please make a your selection (1-3): ";
+	cin >>  * x;
 }
-// Displays Game Rules
+// Displays user selection.
+void menuChoiceSelection(int x)
+{
+	switch(x)
+	{
+		case 1:
+			startGame();
+			break;
+		case 2:
+			viewRules();
+			break;
+		case 3:
+			viewCredits();
+			break;
+		default :
+			exit;
+	}
+	system("pause");
+}
+
+// Starts the game.
+void startGame()
+{
+	system("cls");
+	cout << "Play option is currently currently unavailable." << endl;
+}
+// Displays Game Rules.
 void viewRules()
 {
+	system("cls");
 	cout << "- Sudoku is played over a 9x9 grid, divided to 3x3 sub grids called \"regions\"" << endl
 		 << "- Sudoku begins with some of the grid cells already filled with numbers" << endl
 		 << "- The object of Sudoku is to fill the other empty cells with numbers between\n1 and 9 (1 number only in each cell) according the following guidelines:" << endl << endl
@@ -59,12 +114,13 @@ void viewRules()
 		 << "\t3. Number can appear only once on each region" << endl << endl;
 
 }
-// Validates menuChoice thats 1-4 and not a character.
-int menuChoiceValidation(int x)
+// View program credits
+void viewCredits()
 {
-	
-	return x;
+	system("cls");
+	cout << "Credits option is currently unavailable." << endl;
 }
+// Display current graph.
 void displayGraph(int array[][9])
 {
 	/*
